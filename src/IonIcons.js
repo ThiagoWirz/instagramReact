0d4a4249827e9473e3537c109d88d6eafe9df841
Thiago <thiagowirzdemoura@gmail.com>
2021-12-03 21:12:56 -0300

    Add Navbar

---
+++ src/IonIcons.js
@@ -0,0 +1,3 @@
+export default function IonIcons(props){
+    return <ion-icon name={props.name} />;
+}
---
+++ src/Navbar.js
@@ -0,0 +1,36 @@
+import IonIcons from "./IonIcons"
+
+export default function Navbar(){
+    const icons = ["paper-plane-outline", "compass-outline", "heart-outline", "person-outline"]
+    return (
+        <div class="navbar">
+            <div class="container">
+                <div class="logo">
+                    <ion-icon name="logo-instagram"/>
+                    <div class="separador"></div>
+                    <img src="assets/img/logo.png"  alt =""/>
+                </div>
+
+                <div class="logo-mobile">
+                    <ion-icon name="logo-instagram"/>
+                </div>
+
+                <div class="instagram-mobile">
+                    <img src="assets/img/logo.png" alt =""/>
+                </div>
+  
+                <div class="pesquisa">
+                    <input type="text" placeholder="Pesquisar" />
+                </div>
+  
+                <div class="icones">
+                    {icons.map((icon) => <IonIcons name={icon}/> )}
+                </div>
+
+                <div class="icones-mobile">
+                    <ion-icon name="paper-plane-outline"/>
+                </div>
+           </div>
+        </div>
+    )
+}
